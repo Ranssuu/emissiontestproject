4a28c8d3338a0f8f061d2266734078087e90e424
lnztumala <lnztumala1@gmail.com>
2024-03-31 14:59:01 +0800

    adminhome

---
+++ Home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="CSS/home.css">
+<title>Admin Dashboard</title>
+</head>
+<body>
+
+<div class="sidebar">
+  <div class="admin-header">ADMIN</div>
+  <div class="menu-section">
+    <div class="menu-header">MENU</div>
+    <ul>
+      <li class="active"><i class="icon-dashboard"></i>Dashboard</li>
+      <li><i class="icon-user-info"></i>User Information</li>
+      <li><i class="icon-print-result"></i>Print Result</li>
+      <li><i class="icon-customer-view"></i>Customer View</li>
+    </ul>
+  </div>
+  <div class="menu-section">
+    <div class="menu-header">OTHERS</div>
+    <ul>
+      <li><i class="icon-settings"></i>Settings</li>
+      <li><i class="icon-accounts"></i>Accounts</li>
+      <li><i class="icon-help"></i>Help</li>
+    </ul>
+  </div>
+</div>
+
+</body>
+</html>
