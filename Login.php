a85fca95f86cde0acf4673daffd222dc7e97bd5d
lnztumala <lnztumala1@gmail.com>
2024-03-31 22:18:28 +0800

    Updated

--- Login.php
+++ Login.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
     <link rel="stylesheet" href="CSS/login.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap">
 </head>
 <body>
     <div class="container">
